1909705088b16037f7d6fa6002a6f3b36f09fc72
sunu <psw261@gmail.com>
2018-01-16 00:15:53 +0900

    init commit EncoderCNN_to_AttnDecoderCNN model

---
+++ model.py
@@ -0,0 +1,69 @@
+'''
+Source code for an attention based image caption generation system described
+in:
+Show, Attend and Tell: Neural Image Caption Generation with Visual Attention
+International Conference for Machine Learning (2015)
+http://arxiv.org/abs/1502.03044
+'''
+
+import torch
+import torch.nn as nn
+import torchvision.models as models
+from torch.nn.utils.rnn import pack_padded_sequence
+from torch.autograd import Variable
+
+class EncoderCNN(nn.Module):
+    def __init__(self, embed_size):
+        super(EncoderCNN, self).__init__()
+        resnet = models.resnet152(pretrained=True)
+        modules = list(resnet.children())[:-1]      # delete the last fc layer.
+        self.resnet = nn.Sequential(*modules)
+        self.linear = nn.Linear(resnet.fc.in_features , embed_size)
+        self.bn = nn.BatchNorm1d(embed_size, momentum=0.01)
+        self.init_weights()
+
+    def init_weights(self):
+        self.linear.weight.data.normal_(0.0, 0.2)
+        self.linear.bias.data.fill_(0)
+
+    def forward(self, images):
+        features = self.resnet(images)
+        features = Variable(features.data)
+        features = features.view(features.size(0), -1)
+        features = self.bn(self.linear(features))
+        return features
+
+
+class AttnDecoderRNN(nn.Module):
+    def __init__(self, embed_size, hidden_size, vocab_size, num_layers):
+        super(AttnDecoderRNN, self).__init__()
+        self.embed = nn.Embedding(vocab_size, embed_size)
+        self.lstm = nn.LSTM(embed_size, hidden_size, num_layers, batch_first=True)
+        self.linear = nn.Linear(hidden_size, vocab_size)
+        self.init_weights()
+
+    def init_weights(self):
+        self.embed.weight.data.uniform_(-0.1, 0.1)
+        self.linear.weight.data.uniform_(-0.1, 0.1)
+        self.linear.bias.data.fill_(0)
+
+    def forward(self, features, captions, lengths):
+        embeddings = self.embed(captions)
+        embeddings = torch.cat((features.unsqueeze(1), embeddings), 1)
+        packed = pack_padded_sequence(embeddings, lengths, batch_first=True)
+        hidden, _ = self.lstm(packed)
+        outputs = self.linear(hidden[0])
+        return outputs
+
+    def sample(self, features, states=None):
+        sampled_ids = []
+        inputs = features.unsqueeze(1)
+        for i in range(20):                                      # maximum sampling length
+            hiddens, states = self.lstm(inputs, states)          # (batch_size, 1, hidden_size),
+            outputs = self.linear(hiddens.squeeze(1))            # (batch_size, vocab_size)
+            predicted = outputs.max(1)[1]
+            sampled_ids.append(predicted)
+            inputs = self.embed(predicted)
+            inputs = inputs.unsqueeze(1)                         # (batch_size, 1, embed_size)
+        sampled_ids = torch.cat(sampled_ids, 0)                  # (batch_size, 20)
+        return sampled_ids.squeeze()
